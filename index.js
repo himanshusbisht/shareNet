d3785ee86bdec600cab6e9dba7d3a93d071bee44
himanshusbisht <98160837+himanshusbisht@users.noreply.github.com>
2024-07-10 00:30:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors({
-  origin:["https://share-net-front-end.vercel.app/"],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials:true,
-          }));
-
+app.use(cors());
+// app.use(cors({
+//   origin:["https://share-net-front-end.vercel.app/"],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   credentials:true,
+//           }));
 // Static assets (e.g., images)
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
