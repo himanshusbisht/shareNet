dafb2a535b014f383174e5d05379576423001f23
himanshusbisht <98160837+himanshusbisht@users.noreply.github.com>
2024-07-11 11:52:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config(); // Load environment variables from .env file
 const app = express();
 app.use(cors({
-  origin:["https://share-net-front-end.vercel.app/"],
+  origin:["https://share-net-front-end.vercel.app"],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials:true,
           }));
